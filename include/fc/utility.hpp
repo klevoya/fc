e469583383b2ddf363549e76c7d21405560ea6e5
MrToph <christoph.michel.1992@gmail.com>
2020-05-11 12:15:45 +0400

    Increase payload size and return limit

--- include/fc/utility.hpp
+++ include/fc/utility.hpp
@@ -14,8 +14,9 @@
 #define NO_RETURN __attribute__((noreturn))
 #endif
 
-#define MAX_NUM_ARRAY_ELEMENTS (1024*1024)
-#define MAX_SIZE_OF_BYTE_ARRAYS (20*1024*1024)
+// @cmichel: increased by 10x because push_transaction serializes returned snapshot from variant 
+#define MAX_NUM_ARRAY_ELEMENTS (10*1024*1024)
+#define MAX_SIZE_OF_BYTE_ARRAYS (20*10*1024*1024)
 
 //namespace std {
 //  typedef decltype(sizeof(int)) size_t;
